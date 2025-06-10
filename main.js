5f763f575a9b0cf0d693218545589239050a17b5
abubakr587 <faxriddinovabubakr36@gmail.com>
2025-06-10 16:20:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+  // const body = document.querySelector('body');
+  
+// function lightMode(){
+//       body.innerHTML.backgroundColor = "white";
+    
+//     }
+
+//   function darkMode(){
+//       body.innerHTML.backgroundColor = "black";
+
+//     }
+    function darkMode() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
+
+function lightMode() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+}
